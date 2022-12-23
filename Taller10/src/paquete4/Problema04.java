4bc6423b8a62b9667a7460af1bd2c58e27f30582
Erick gaona <Erickgaona625@gmail.com>
2022-12-23 10:15:17 -0500

    Trabajo grupal

--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -13,29 +13,32 @@ import java.util.Scanner;
  * @author reroes
  */
 public class Problema04 {
+
     public static void main(String[] args) {
-    Scanner entrada = new Scanner(System.in);
-    entrada.useLocale(Locale.US);
-    
-    //double[][] dato = new double[2][2];
-    double[] numeros = new double[5];
-    int i;
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        //double[][] dato = new double[2][2];
+        double[][] dato = new double[2][2];
+        int i;
+        int j;
         System.out.println("Ingrese datos decimales");
-        for (i = 0; i < numeros.length; i++) {
-            System.out.print("numeros[" + i + "]= ");
-            numeros[i]=entrada.nextDouble();
+        for (i = 0; i < dato.length; i++) {
+            for ( j = 0; j < dato[i].length; j++) {
+                System.out.print("numeros[" + i + "]"+"[" +j+ "]"+" = ");
+                dato[i][j] = entrada.nextDouble();
+                
+            }
         }
-    
-    
-        
-    
-        
+            for ( i = 0; i < dato.length; i++) {
+                for (j = 0; j < dato[i].length; j++) {
+                    System.out.println(dato[i][j]);   
+                    
+                }
+            }
+        }
        
         
-        ;
-        
-    
-    
-    
-}
-}
+
+    }
+
